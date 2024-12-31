40b227835146afb7571d2f235979ccf96b850aa8
codemario <thdtkgus1234@gmail.com>
2024-12-31 23:06:19 +0900

    make simple html and load pakage for login function

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-console.log('app.js 실행됨');
-try {
-    const express = require('express');
-    const dotenv = require('dotenv');
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
 
-    dotenv.config();
+dotenv.config();
 
-    const app = express();
+const app = express();
 
-    app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
-    app.get('/', (req, res) => {
-        res.send('Hello World!');
-    });
 
-    app.listen(app.get('port'), () => {
-        console.log(app.get('port'), '번 포트에서 대기 중');
-    });
-} catch (err) {
-    console.error('An error occurred:', err);
-    process.exit(1);
-}
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
