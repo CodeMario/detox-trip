4dd7aa1f11e51eb7e17830731f3de9be9074fd48
codemario <thdtkgus1234@gmail.com>
2025-01-01 23:03:10 +0900

    connect node with mysql

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const { sequelize } = require('./models');
 
 dotenv.config();
 
@@ -8,6 +11,13 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+sequelize.sync({ force:false })
+    .then(() => {
+        console.log('데이터베이스 연결 성공');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'));
